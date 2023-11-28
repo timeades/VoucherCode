593c46c381e23064b9acb5632c09c711cbc9f9eb
timeades <tim.eades@mac.com>
2023-11-28 16:33:57 +0000

    add selectors

---
+++ utils/constants/urls.ts
@@ -0,0 +1,2 @@
+export const baseUrl =
+    process.env.BASE_URL || 'https://www.vouchercodes.co.uk/';
---
+++ utils/search/selectors.ts
@@ -0,0 +1,7 @@
+export enum HomePageComponent {
+    homepageSearchInput = '[id="appSearchPlaceholderInput"]',
+    searchInput = '[data-qa="el:search search:"]',
+    locationInput = '[data-qa="el:locationDropdown"]',
+    dayInput = '[name="day-select"]',
+    peopleInput = '[name="people-select"]',
+}
